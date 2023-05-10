8b36af9f820d21c9acb714295eb8c5ae17d25b4b
jayy118 <hyun18_@naver.com>
2023-05-10 11:55:14 +0900

    gsap 구현

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,21 @@
 import Head from "next/head";
+import { gsap } from "gsap/dist/gsap";
+import ScrollTrigger from 'gsap/dist/ScrollTrigger'
+import { useEffect } from 'react';
 import CustomComponents from "../components/custom/Custom-components";
 
 export default function Home() {
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+    gsap.from(".img-fluid", {
+      scrollTrigger: {
+      trigger: ".img-fluid",
+      toggleActions: "restart none reverse none",
+      //markers: true,
+      start: "top center"
+    },
+    rotation: 100, x: 300, duration: 1, opacity: 0});
+  }, []);
   return (
     <div>
       <Head>
