c6cc8bf0ecf5c818a768dbfc4daa76cf1a57660d
seoyeong12 <qlqltjdud@gmail.com>
2023-05-23 13:50:10 +0900

    content page 1차

--- pages/content.js
+++ pages/content.js
@@ -9,11 +9,26 @@ function Content() {
                 <p id="booktitle">책 제목1</p>
                 <p id="bookcontent">저자나 태그</p>
             </div>
-            
         </div>
     );
+    const movieitem = (
+        <div id="movieitem">
+            <div id="movieimg">
+                image
+            </div>
+            <div id="moviesummary">
+                <p id="movietitle">영화 제목1</p>
+                <p id="moviecontent">저자나 태그</p>
+            </div>
+        </div>
+    )
     return (
-        <div id="booklist">{bookitem}</div>
+        <div id="body">
+            <p id="title">추천 콘텐츠</p>
+            <p id="ex">콘텐츠 추천기능을 체험해보세요.</p>
+            <div id="booklist">{bookitem}</div>
+            <div id="movielist">{movieitem}</div>
+        </div>
     );
 }
 
