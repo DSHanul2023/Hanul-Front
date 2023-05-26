599afe8d5419428eb22ba0be9bded4b41a4438b4
seoyeong12 <qlqltjdud@gmail.com>
2023-05-26 22:13:34 +0900

    scss로 수정, reactstrap 이용

---
+++ components/custom/sections/listpaginationcomponent.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Pagination,PaginationItem,PaginationLink } from 'reactstrap';
+const ListPagination = () => {
+    return(
+        <Pagination className="listpagination" aria-label="Page navigation example">
+            <PaginationItem>
+            <PaginationLink previous href="#" />
+            </PaginationItem>
+            <PaginationItem>
+            <PaginationLink href="#">
+                1
+            </PaginationLink>
+            </PaginationItem>
+            <PaginationItem>
+            <PaginationLink href="#">
+                2
+            </PaginationLink>
+            </PaginationItem>
+            <PaginationItem>
+            <PaginationLink href="#">
+                3
+            </PaginationLink>
+            </PaginationItem>
+            <PaginationItem>
+            <PaginationLink href="#">
+                4
+            </PaginationLink>
+            </PaginationItem>
+            <PaginationItem>
+            <PaginationLink href="#">
+                5
+            </PaginationLink>
+            </PaginationItem>
+            <PaginationItem>
+            <PaginationLink next href="#" />
+            </PaginationItem>
+        </Pagination>
+    );
+};
+
+export default ListPagination;
