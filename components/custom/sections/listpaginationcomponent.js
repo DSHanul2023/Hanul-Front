e9ed4296ccb125695f4e80e5d67a03c98d4c7b32
jiho-par <hkp314518@duksung.ac.kr>
2023-07-31 14:44:05 +0900

    Item(TMDB) Backend connect

--- components/custom/sections/listpaginationcomponent.js
+++ components/custom/sections/listpaginationcomponent.js
@@ -1,41 +1,25 @@
 import React from 'react';
-import { Pagination,PaginationItem,PaginationLink } from 'reactstrap';
-const ListPagination = () => {
-    return(
-        <Pagination className="listpagination" aria-label="Page navigation example">
-            <PaginationItem>
-            <PaginationLink previous href="#" />
-            </PaginationItem>
-            <PaginationItem>
-            <PaginationLink href="#">
-                1
-            </PaginationLink>
-            </PaginationItem>
-            <PaginationItem>
-            <PaginationLink href="#">
-                2
-            </PaginationLink>
-            </PaginationItem>
-            <PaginationItem>
-            <PaginationLink href="#">
-                3
-            </PaginationLink>
-            </PaginationItem>
-            <PaginationItem>
-            <PaginationLink href="#">
-                4
-            </PaginationLink>
-            </PaginationItem>
-            <PaginationItem>
-            <PaginationLink href="#">
-                5
-            </PaginationLink>
-            </PaginationItem>
-            <PaginationItem>
-            <PaginationLink next href="#" />
-            </PaginationItem>
-        </Pagination>
-    );
+import { Pagination, PaginationItem, PaginationLink } from 'reactstrap';
+
+// ContentList 컴포넌트에서 전달받은 데이터에 따라 동적으로 페이지네이션을 생성
+const ListPagination = ({ totalPages }) => {
+  const pageNumbers = Array.from({ length: totalPages }, (_, index) => index + 1);
+
+  return (
+    <Pagination className="listpagination" aria-label="Page navigation example">
+      <PaginationItem>
+        <PaginationLink previous href="#" />
+      </PaginationItem>
+      {pageNumbers.map((pageNumber) => (
+        <PaginationItem key={pageNumber}>
+          <PaginationLink href="#">{pageNumber}</PaginationLink>
+        </PaginationItem>
+      ))}
+      <PaginationItem>
+        <PaginationLink next href="#" />
+      </PaginationItem>
+    </Pagination>
+  );
 };
 
 export default ListPagination;
--- my-app/next.config.js
+++ my-app/next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ['assets.example.com', 'image.tmdb.org'],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
