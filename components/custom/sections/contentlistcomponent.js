ab1ba25bacf4f9b9421440dd3a6daf7ff464ce02
pjho4746 <pjho4746@gmail.com>
2023-07-31 14:44:05 +0900

    Item(TMDB) Backend connect

--- components/custom/sections/contentlistcomponent.js
+++ components/custom/sections/contentlistcomponent.js
@@ -1,22 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Container } from 'reactstrap';
 import ItemComponent from './itemcomponent';
-import ListPagination from './listpaginationcomponent'
+import ListPagination from './ListPaginationComponent';
 
 const ContentList = () => {
-    return(
-            <Container className="contentlist">
-            <p className="contenttitle">추천 콘텐츠</p>
-            <p className="contentex">콘텐츠 추천기능을 체험해보세요.</p>
-            <Container className="booklist">
-                <ItemComponent/><ItemComponent/><ItemComponent/><ItemComponent/>
-            </Container>
-            <Container className="movielist"><ItemComponent/></Container>
-            <div className="listpagination">
-                <ListPagination/>
-            </div>
-            </Container>
-    );
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+
+  const fetchMovies = async () => {
+    try {
+      const response = await fetch('http://localhost:8080/items/all');
+      const data = await response.json();
+
+      // 프론트엔드에서 사용하는 데이터 구조에 posterUrl 속성을 추가
+      const moviesWithPosterUrl = data.map(movie => ({
+        ...movie,
+        posterUrl: movie.posterUrl ? `https://image.tmdb.org/t/p/w500${movie.posterUrl}` : '', // 영화 포스터 이미지 URL
+      }));
+
+      setMovies(moviesWithPosterUrl);
+    } catch (error) {
+      console.error('영화 데이터를 가져오는 중 오류 발생:', error);
+    }
+  };
+
+  return (
+    <Container>
+      <ItemComponent movies={movies} />
+      <div className="listpagination">
+        <ListPagination />
+      </div>
+    </Container>
+  );
 };
 
 export default ContentList;
--- components/custom/sections/itemcomponent.js
+++ components/custom/sections/itemcomponent.js
@@ -1,16 +1,38 @@
 import React from 'react';
-import { Container } from 'reactstrap';
+import { Container, Row, Col } from 'reactstrap';
+import Image from 'next/image';
 
-const ItemComponent = () => {
-    return(
-        <div className="item" height="200px" width="150px">
-            <div className="itemimg" />
-            <Container className="itemsummary">
-                <p className="itemtitle">제목1</p>
-                <p className="itemcontent">저자나 태그</p>
-            </Container>
-        </div>
-    );
+const ItemComponent = ({ movies }) => {
+  // 2차원 배열로 변환 (페이지마다 10개의 영화 데이터를 가지고 있다고 가정)
+  const chunkedMovies = [];
+  const itemsPerRow = 5;
+  for (let i = 0; i < movies.length; i += itemsPerRow) {
+    chunkedMovies.push(movies.slice(i, i + itemsPerRow));
+  }
+
+  return (
+    <Container className="contentlist">
+      <p className="contenttitle">추천 콘텐츠</p>
+      <p className="contentex">콘텐츠 추천 기능을 체험해보세요.</p>
+      <Container className="movielist">
+        {chunkedMovies.map((row, rowIndex) => (
+          <Row key={rowIndex}>
+            {row.map((movie, colIndex) => (
+              <Col key={colIndex} className="item" style={{ height: "200px", width: "150px" }}>
+                <div className="itemimg" style={{ width: "100%", height: "100%" }}>
+                  <Image src={movie.posterUrl} alt={movie.itemNm} layout="fill" objectFit="cover" />
+                </div>
+                <Container className="itemsummary">
+                  <p className="itemtitle">{movie.itemNm}</p>
+                  <p className="itemcontent">{movie.itemDetail}</p>
+                </Container>
+              </Col>
+            ))}
+          </Row>
+        ))}
+      </Container>
+    </Container>
+  );
 };
 
 export default ItemComponent;
