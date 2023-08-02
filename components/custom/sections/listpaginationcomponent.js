23756586ba0f9f458a20e913bf750a04ada0a14b
pjho4746 <pjho4746@gmail.com>
2023-08-02 13:12:13 +0900

    pagination 관련 수정

--- components/custom/sections/listpaginationcomponent.js
+++ components/custom/sections/listpaginationcomponent.js
@@ -1,22 +1,29 @@
 import React from 'react';
 import { Pagination, PaginationItem, PaginationLink } from 'reactstrap';
 
-// ContentList 컴포넌트에서 전달받은 데이터에 따라 동적으로 페이지네이션을 생성
-const ListPagination = ({ totalPages }) => {
+const ListPagination = ({ totalPages, currentPage, onPageChange }) => {
   const pageNumbers = Array.from({ length: totalPages }, (_, index) => index + 1);
+
+  const handlePageChange = (page) => {
+    // 페이지 번호가 최소값과 최대값을 벗어나지 않도록 검사
+    const nextPage = Math.min(Math.max(page, 1), totalPages + 1);
+    onPageChange(nextPage);
+  };
 
   return (
     <Pagination className="listpagination" aria-label="Page navigation example">
-      <PaginationItem>
-        <PaginationLink previous href="#" />
+      <PaginationItem disabled={currentPage === 1}>
+        <PaginationLink previous href="#" onClick={() => handlePageChange(currentPage - 1)} />
       </PaginationItem>
       {pageNumbers.map((pageNumber) => (
-        <PaginationItem key={pageNumber}>
-          <PaginationLink href="#">{pageNumber}</PaginationLink>
+        <PaginationItem key={pageNumber} active={pageNumber === currentPage}>
+          <PaginationLink href="#" onClick={() => handlePageChange(pageNumber)}>
+            {pageNumber}
+          </PaginationLink>
         </PaginationItem>
       ))}
-      <PaginationItem>
-        <PaginationLink next href="#" />
+      <PaginationItem disabled={currentPage === totalPages}>
+        <PaginationLink next href="#" onClick={() => handlePageChange(currentPage + 1)} />
       </PaginationItem>
     </Pagination>
   );
