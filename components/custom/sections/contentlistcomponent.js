484a0a4e03faf469f90fafc91c0bdc1a42a07861
jiho-par <hkp314518@duksung.ac.kr>
2023-08-02 13:12:13 +0900

    pagination 관련 수정

--- components/custom/sections/contentlistcomponent.js
+++ components/custom/sections/contentlistcomponent.js
@@ -5,33 +5,37 @@ import ListPagination from './ListPaginationComponent';
 
 const ContentList = () => {
   const [movies, setMovies] = useState([]);
+  const [totalPages, setTotalPages] = useState(1);
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 10;
 
   useEffect(() => {
-    fetchMovies();
-  }, []);
+    fetchMovies(currentPage);
+  }, [currentPage]);
 
-  const fetchMovies = async () => {
+  const fetchMovies = async (page) => {
     try {
-      const response = await fetch('http://localhost:8080/items/all');
-      const data = await response.json();
-
-      // 프론트엔드에서 사용하는 데이터 구조에 posterUrl 속성을 추가
-      const moviesWithPosterUrl = data.map(movie => ({
-        ...movie,
-        posterUrl: movie.posterUrl ? `https://image.tmdb.org/t/p/w500${movie.posterUrl}` : '', // 영화 포스터 이미지 URL
-      }));
-
-      setMovies(moviesWithPosterUrl);
+      const response = await fetch(`http://localhost:8080/items/all?page=${page}&size=${itemsPerPage}`);
+        if (!response.ok) {
+            throw new Error('데이터를 가져오는 중 오류 발생');
+        }
+        const data = await response.json();
+        const totalItems = Number(response.headers.get('Total-Count'));
+        const totalPages = Math.ceil(totalItems / itemsPerPage); // Math.ceil을 사용하여 올림 (10개 이하인 경우도 고려)
+        setTotalPages(totalPages);
+        setMovies(data);
     } catch (error) {
-      console.error('영화 데이터를 가져오는 중 오류 발생:', error);
+      console.error('데이터를 가져오는 중 오류 발생:', error);
+      // 오류 처리: 오류 메시지 / 대체 콘텐츠 보여줌
     }
-  };
+};
+
 
   return (
     <Container style={{ position: 'relative' }}>
       <ItemComponent movies={movies} />
       <div className="listpagination" style={{ position: 'absolute', bottom: '20px', left: '50%', transform: 'translateX(-50%)'}}>
-        <ListPagination />
+        <ListPagination totalPages={totalPages} currentPage={currentPage} onPageChange={setCurrentPage} />
       </div>
     </Container>
   );
--- components/custom/sections/itemcomponent.js
+++ components/custom/sections/itemcomponent.js
@@ -17,14 +17,12 @@ const ItemComponent = ({ movies }) => {
           <Row key={rowIndex} style={{ marginTop: "40px" }}>
             {row.map((movie, colIndex) => (
               <Col key={colIndex} className="item" style={{ height: "300px", width: "200px" }}>
-                <div className="itemimg" style={{ width: "100%", height: "80%" }}>s
-                  <Image
-                    src={movie.posterUrl} // 이미지 URL은 여기에 설정
-                    alt={movie.itemNm}
-                    layout="fill" // layout 속성을 사용하여 이미지 크기 조정 방식 설정
-                    objectFit="cover" // 이미지 크기가 부모 컨테이너에 맞게 조정되도록 설정
-                    priority // priority 속성을 추가하여 이미지가 우선적으로 로딩되도록 설정
-                  />
+                <div className="itemimg" style={{ width: "100%", height: "80%" }}>
+                <Image
+                src={movie.posterUrl}
+                alt={movie.itemNm}
+                layout="fill"
+                objectFit="cover"/>
                 </div>
                 <Container className="itemsummary">
                 <p className="itemtitle" style={{ marginTop: "70px", fontSize: "13px" }}>{movie.itemNm}</p>
