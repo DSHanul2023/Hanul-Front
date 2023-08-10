6efb89504e9d9638317168affda2416d225fd209
seoyeong <qlqltjdud@gmail.com>
2023-08-11 02:18:21 +0900

    커뮤니티 댓글 기능

--- components/custom/sections/boardinsidecomponent.js
+++ components/custom/sections/boardinsidecomponent.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Container, Button, FormGroup, Input, Label, Form } from 'reactstrap';
+import { Container, Button, FormGroup, Input, Label, Form, CardImg } from 'reactstrap';
 import { useRouter } from "next/router";
 import { Card, CardTitle, CardText, Row, Col } from 'reactstrap';
 
@@ -13,6 +13,12 @@ const BoardInsideComponent = () => {
     const [title, setTitle] = useState("");
     const [contents, setContents] = useState("");
     const [selected, setSelected] = useState(1);
+    const [comments, setComments] = useState([]);
+    const [newComment, setNewComment] = useState("");
+    const [isCommentEditMode,setIsCommentEditMode]=useState(false);
+    const [editingCommentId, setEditingCommentId] = useState(null);
+    const [editingCommentText, setEditingCommentText] = useState("");
+
     useEffect(() => {
         const accessToken = localStorage.getItem("ACCESS_TOKEN");
         if (!accessToken) {
@@ -22,6 +28,7 @@ const BoardInsideComponent = () => {
             handleFetchBoardData(accessToken);
             setToken(accessToken);
         }
+        fetchComments(accessToken);
     }, []);
     const handleFetchBoardData = async (accessToken) => {
         console.log(accessToken);
@@ -83,7 +90,6 @@ const BoardInsideComponent = () => {
 
     const handleDeleteClick = async () => {
         const accessToken = token;
-        console.log(accessToken);
         try {
             const response = await fetch(`http://localhost:8080/board`, {
                 method: 'DELETE',
@@ -103,7 +109,117 @@ const BoardInsideComponent = () => {
             console.error('Error:', error);
             }
         };
+
+
+    //댓글기능
+    const fetchComments = async (accessToken) => {
+        try {
+            const response = await fetch(`http://localhost:8080/comments/${id}`, {
+                method: "GET",
+                headers: {
+                    'Authorization': `Bearer ${accessToken}`,
+                    'Content-Type': 'application/json'
+                }
+            });
+            if (response.ok) {
+                const data = await response.json();
+                setComments(data.data || []); // If data is null or undefined, set an empty array
+            }
+        } catch (error) {
+            console.error("댓글 가져오기 오류:", error);
+        }
+    };
+    const handleCommentSubmit = async (e) => {
+        e.preventDefault();
+    
+        try {
+            const response = await fetch(`http://localhost:8080/comments`, {
+                method: "POST",
+                headers: {
+                    'Authorization': `Bearer ${token}`,
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    boardId: id,
+                    text: newComment
+                })
+            });
+            if (response.ok) {
+                console.log("추가 완료");
+                // 댓글이 성공적으로 추가되었으면 업데이트된 댓글을 가져옵니다.
+                fetchComments(token);
+                setNewComment(""); // 댓글 입력란을 지웁니다.
+            }
+        } catch (error) {
+            console.error("댓글 추가 오류:", error);
+        }
+    };
+    const handleEditComment = (comment) => {
+        setEditingCommentId(comment.id);
+        setEditingCommentText(comment.text);
+        setIsCommentEditMode(true);
+    };
+    
+    // 댓글 수정 취소 함수
+    const handleCancelEdit = () => {
+        setEditingCommentId(null);
+        setEditingCommentText('');
+        setIsCommentEditMode(false);
+    };
+    
+    // 댓글 수정 내용 변경 함수
+    const handleEditingCommentChange = (e) => {
+        setEditingCommentText(e.target.value);
+    };    
         
+    // 댓글 업데이트 처리 함수
+    const handleUpdateComment = async (commentId) => {
+        const accessToken = token;
+    
+        try {
+            const response = await fetch(`http://localhost:8080/comments`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${accessToken}`,
+                },
+                body: JSON.stringify({
+                    text: editingCommentText, 
+                    id:commentId
+                }),
+            });
+            if (response.ok) {
+                console.log('Comment updated');
+                fetchComments(token); // 업데이트된 댓글 목록 가져오기
+                handleCancelEdit(); // 수정 완료 후 수정 모드 종료
+            }
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    };
+    // 댓글 삭제 처리 함수
+    const handleDeleteComment = async (commentId) => {
+    const accessToken = token;
+    try {
+        const response = await fetch(`http://localhost:8080/comments`, {
+            method: 'DELETE',
+            headers: {
+                'Authorization': `Bearer ${accessToken}`,
+                'Content-Type': 'application/json'
+            },
+            body:JSON.stringify({
+                id:commentId
+            }),
+        });
+        if (response.ok) {
+            console.log('Comment deleted');
+            // 댓글 삭제 후 업데이트된 댓글 목록을 가져옵니다.
+            fetchComments(token);
+        }
+    } catch (error) {
+        console.error('Error:', error);
+    }
+};
     return (
         <div className='boardCreate'>
             <Container>
@@ -148,7 +264,7 @@ const BoardInsideComponent = () => {
                         <Button color="secondary" onClick={handleCancelClick}>취소</Button>
                     </Form>
                 ) : (
-                    <> <Card style={{ backgroundColor: "#ffffff" }}>
+                    <> <Card>
                     <Container>
                         <Row className='card-main'>
                             <Col md="11">
@@ -164,7 +280,49 @@ const BoardInsideComponent = () => {
                                 <CardText>{boardData.contents}</CardText>
                             </Col>
                             <Col className='board-right'>
-                                <CardText></CardText>
+                            {boardData.image && (
+                                <CardImg src={boardData.image} alt="{boardData.imageTitle}" />
+                            )}
+                            </Col>
+                        </Row>
+                        <hr />
+                        <Row>
+                            <Col>
+                            <h3>댓글</h3>
+                            {comments ? (
+                                comments.map(comment => (
+                                    <div key={comment.id}>
+                                        {isCommentEditMode && editingCommentId === comment.id ? (
+                                            // 댓글 수정 폼
+                                            <div>
+                                                <textarea
+                                                    value={editingCommentText}
+                                                    onChange={handleEditingCommentChange}
+                                                />
+                                                <Button color="success" onClick={() => handleUpdateComment(comment.id)}>수정 완료</Button>
+                                                <Button color="secondary" onClick={handleCancelEdit}>취소</Button>
+                                            </div>
+                                        ) : (
+                                    // 댓글 내용 보기 모드
+                                    <div>
+                                        <p>{comment.text} 작성자: {comment.author} 작성날짜 {comment.date}</p>
+                                        {comment.canEdit && (
+                                            <>
+                                                <Button color="link" onClick={() => handleEditComment(comment)}>수정</Button>
+                                                <Button color="link" onClick={() => handleDeleteComment(comment.id)}>삭제</Button>
+                                            </>
+                                        )}
+                                        <hr />
+                                    </div>
+                                )}
+                            </div>
+                        ))
+                                ) : (
+                                    <p>No comments available.</p>
+                                )}
+                                <form onSubmit={handleCommentSubmit}>
+                                    {/* 댓글 입력란과 전송 버튼 */}
+                                </form>
                             </Col>
                         </Row>
                     </Container>
@@ -176,6 +334,20 @@ const BoardInsideComponent = () => {
                 )} </Card>
                     </>
                 )}
+                
+                <form onSubmit={handleCommentSubmit}>
+                    <FormGroup>
+                        <Label htmlFor="comment">댓글 작성</Label>
+                        <Input
+                            type="textarea"
+                            className="form-control"
+                            id="comment"
+                            value={newComment}
+                            onChange={(e) => setNewComment(e.target.value)}
+                        />
+                    </FormGroup>
+                    <Button color="primary" type="submit">댓글 작성</Button>
+                </form>
             </Container>
         </div>
     );
