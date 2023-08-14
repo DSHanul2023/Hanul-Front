b4e5e9fd7dc0f250ec6f7032062ada56a48a0850
jiho-par <hkp314518@duksung.ac.kr>
2023-08-14 16:08:47 +0900

    moviedetail 1차

--- pages/login.js
+++ pages/login.js
@@ -1,6 +1,4 @@
 import Head from "next/head";
-import CommunityComponents from "../components/custom/Community-components";
-import ChatComponent from "../components/custom/sections/chatcomponent";
 import LoginComponent from "../components/custom/sections/logincomponent";
 
 export default function Login() {
