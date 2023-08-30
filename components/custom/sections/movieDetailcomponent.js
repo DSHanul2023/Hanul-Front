51ebebfb16c22ee8870d3cf6ca48738538ea7d1a
pjho4746 <pjho4746@gmail.com>
2023-08-30 16:18:09 +0900

    영화 상세 페이지 4차

--- components/custom/sections/movieDetailcomponent.js
+++ components/custom/sections/movieDetailcomponent.js
@@ -7,27 +7,29 @@ const MovieDetailComponent = ({ movieId }) => {
   const router = useRouter();
   const [movie, setMovie] = useState(null);
 
-  useEffect(() => {
-    if (!movieId) {
-      return; // movieId가 없으면 아무 작업도 수행하지 않음
-    }
+useEffect(() => {
+  if (!movieId) {
+    return;
+  }
 
-    // 여기서 실제로 API 요청 등을 통해 영화 데이터를 가져와서 setMovie로 설정해야 합니다.
-    // 아래는 예시로서 fetch를 사용해서 데이터를 가져온다고 가정합니다.
-    fetch(`/items/${movieId}`)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json();
-      })
-      .then(data => {
-        setMovie(data);
-      })
-      .catch(error => {
-        console.error('Error while fetching data:', error);
-      });
-  }, [movieId]);
+  console.log('영화 데이터를 가져오는 중...');
+
+  fetch(`/items/${movieId}`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('네트워크 응답이 올바르지 않습니다.');
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log('가져온 영화 데이터:', data);
+      setMovie(data);
+    })
+    .catch(error => {
+      console.error('데이터 가져오기 오류:', error);
+    });
+}, [movieId]);
+
 
   if (!movie) {
     return <p>Loading...</p>;
