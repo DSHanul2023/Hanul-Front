5bf3b344dcaf93f559ede243f4f7cb14ee5ae568
jiho-par <hkp314518@duksung.ac.kr>
2023-08-30 15:33:35 +0900

    영화 상세 페이지 1차

--- pages/content.js
+++ pages/content.js
@@ -1,13 +1,8 @@
-import Head from "next/head";
-import ContentList from '../components/custom/sections/contentlistcomponent';
+import ContentList from "../components/custom/sections/contentlistcomponent";
 
 export default function Content() {
         return (
         <div>
-        <Head>
-          <title>We:lover - Chat</title>
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
         <ContentList />
       </div>
         );
