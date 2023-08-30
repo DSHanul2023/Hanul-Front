f1d16e790c53cc1e694ee1f5b09a6f480aaed24d
pjho4746 <pjho4746@gmail.com>
2023-08-30 16:30:51 +0900

    영화 상세 페이지 완성

--- pages/items/[id].js
+++ pages/items/[id].js
@@ -6,12 +6,10 @@ const MovieDetailPage = () => {
   const router = useRouter();
   const { id } = router.query; // [id]를 통해 동적으로 전달되는 영화 ID
 
-  const movieId = id ? parseInt(id) : null; // 문자열 ID를 숫자로 변환
-
   return (
     <div>
-      {/* MovieDetailComponent로 영화 ID를 전달 */}
-      <MovieDetailComponent movieId={movieId} />
+      {/* MovieDetailComponent로 영화 ID를 문자열로 전달 */}
+      <MovieDetailComponent movieId={id} />
     </div>
   );
 };
