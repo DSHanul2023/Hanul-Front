908b1f4aacb7270c2685905c4af40d800c31503b
jiho-par <hkp314518@duksung.ac.kr>
2023-08-30 16:30:51 +0900

    영화 상세 페이지 완성

--- components/custom/sections/movieDetailcomponent.js
+++ components/custom/sections/movieDetailcomponent.js
@@ -7,29 +7,29 @@ const MovieDetailComponent = ({ movieId }) => {
   const router = useRouter();
   const [movie, setMovie] = useState(null);
 
-useEffect(() => {
-  if (!movieId) {
-    return;
-  }
-
-  console.log('영화 데이터를 가져오는 중...');
-
-  fetch(`/items/${movieId}`)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('네트워크 응답이 올바르지 않습니다.');
-      }
-      return response.json();
-    })
-    .then(data => {
-      console.log('가져온 영화 데이터:', data);
-      setMovie(data);
-    })
-    .catch(error => {
-      console.error('데이터 가져오기 오류:', error);
-    });
-}, [movieId]);
-
+  useEffect(() => {
+    if (!movieId) {
+      return;
+    }
+  
+    console.log('영화 데이터를 가져오는 중...');
+  
+    fetch(`http://localhost:8080/items/${movieId}`)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('네트워크 응답이 올바르지 않습니다.');
+        }
+        return response.json();
+      })
+      .then(data => {
+        console.log('가져온 영화 데이터:', data);
+        setMovie(data);
+      })
+      .catch(error => {
+        console.error('데이터 가져오기 오류:', error);
+      });
+  }, [movieId]);
+  
 
   if (!movie) {
     return <p>Loading...</p>;
