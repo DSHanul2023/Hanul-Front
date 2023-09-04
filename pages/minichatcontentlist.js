5c99efcde57b149dd31d8fb6c9e06341e939cb0a
pjho4746 <pjho4746@gmail.com>
2023-09-04 10:31:42 +0900

    response category 출력

---
+++ pages/minichatcontentlist.js
@@ -0,0 +1,9 @@
+import MinichatContentList from "../components/custom/sections/minichatcontentlistcomponent";
+
+export default function minichatcontentlist() {
+        return (
+        <div>
+        <MinichatContentList />
+      </div>
+        );
+}
