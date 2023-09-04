e19b5ae03e8d534d27bd9228626f9d747c09fc01
jiho-par <hkp314518@duksung.ac.kr>
2023-09-04 19:33:19 +0900

    Minichat 추천 완성

--- components/custom/sections/minichatcontentlistcomponent.js
+++ components/custom/sections/minichatcontentlistcomponent.js
@@ -1,22 +1,41 @@
 import React from 'react';
-import { Container } from 'reactstrap';
+import { Container, Row, Col } from 'reactstrap';
 import Image from 'next/image';
+import Link from 'next/link';
 
 const MinichatContentList = ({ recommendedMovies }) => {
+  // 5개씩 영화 데이터를 묶어 배열 생성
+  const chunkedMovies = [];
+  for (let i = 0; i < recommendedMovies.length; i += 5) {
+    chunkedMovies.push(recommendedMovies.slice(i, i + 5));
+  }
+
   return (
     <Container style={{ position: 'relative' }}>
-      {recommendedMovies && recommendedMovies.map((movie, index) => (
-        <div key={index} style={{ marginBottom: '20px' }}>
-          <h2>{movie.itemNm}</h2>
-          <p>장르: {movie.genreName}</p>
-          <p>상세 정보: {movie.itemDetail}</p>
-          <Image
-            src={movie.posterUrl}
-            alt={movie.itemNm}
-            width={300}
-            height={400}
-          />
-        </div>
+      {chunkedMovies.map((moviesRow, rowIndex) => (
+        <Row key={rowIndex}>
+          {moviesRow.map((movie, colIndex) => (
+            <Col key={colIndex} xs={12} md={2} className="item">
+              <Link href={`/items/${movie.movieId}`} passHref>
+                <div style={{ cursor: 'pointer' }}>
+                  <div className="itemimg" style={{ width: '100%', height: '80%' }}>
+                    <Image
+                      src={movie.posterUrl}
+                      alt={movie.itemNm}
+                      layout="fill"
+                      objectFit="cover"
+                    />
+                  </div>
+                  <Container className="itemsummary">
+                    <p className="itemtitle" style={{ marginTop: '70px', fontSize: '13px' }}>
+                      {movie.itemNm}
+                    </p>
+                  </Container>
+                </div>
+              </Link>
+            </Col>
+          ))}
+        </Row>
       ))}
     </Container>
   );
