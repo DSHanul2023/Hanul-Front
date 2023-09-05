fd1ea11eff40f6750946c32ecb646238a0e3e8cc
jiho-par <hkp314518@duksung.ac.kr>
2023-09-05 16:10:55 +0900

    Minichat 추천 영화 디자인 수정 (5개씩 출력)

--- components/custom/sections/minichatpagination.js
+++ components/custom/sections/minichatpagination.js
@@ -1,19 +1,18 @@
 import React from 'react';
-import { Pagination, PaginationItem, PaginationLink } from 'reactstrap';
+import { Button } from 'reactstrap';
+
+const buttonStyle = {
+  backgroundColor: '#B95C37', // 원하는 배경색
+  borderColor: '#B95C37', // 원하는 테두리 색상
+  color: 'white', // 버튼 텍스트 색상
+  transition: 'filter 0.3s, brightness 0.3s', // 효과 시간을 조절할 수 있는 CSS 속성 추가
+};
+
+const hoverStyle = {
+  filter: 'brightness(1.2)', // 마우스를 올렸을 때 밝아지는 효과
+};
 
 const MinichatPagination = ({ totalPages, currentPage, onPageChange }) => {
-  const visiblePageNumbers = Array.from({ length: totalPages }, (_, index) => index + 1);
-
-  // 현재 페이지를 중심으로 앞 뒤로 5개의 페이지만 보이도록 계산
-  const maxVisiblePages = 5;
-  const middlePage = Math.ceil(maxVisiblePages / 2);
-  let startPage = Math.max(currentPage - middlePage, 1);
-  let endPage = Math.min(startPage + maxVisiblePages - 1, totalPages);
-
-  if (endPage - startPage < maxVisiblePages - 1) {
-    startPage = Math.max(endPage - maxVisiblePages + 1, 1);
-  }
-
   const handlePageChange = (page) => {
     if (page !== currentPage) {
       onPageChange(page);
@@ -21,21 +20,19 @@ const MinichatPagination = ({ totalPages, currentPage, onPageChange }) => {
   };
 
   return (
-    <Pagination className="minichat-pagination" aria-label="Page navigation example">
-      <PaginationItem disabled={currentPage === 1}>
-        <PaginationLink previous href="#" onClick={() => handlePageChange(currentPage - 1)} />
-      </PaginationItem>
-      {visiblePageNumbers.slice(startPage - 1, endPage).map((pageNumber) => (
-        <PaginationItem key={pageNumber} active={pageNumber === currentPage}>
-          <PaginationLink href="#" onClick={() => handlePageChange(pageNumber)}>
-            {pageNumber}
-          </PaginationLink>
-        </PaginationItem>
-      ))}
-      <PaginationItem disabled={currentPage === totalPages}>
-        <PaginationLink next href="#" onClick={() => handlePageChange(currentPage + 1)} />
-      </PaginationItem>
-    </Pagination>
+    <div className="minichat-pagination" aria-label="Page navigation example">
+      <Button
+        style={{ ...buttonStyle, ...(currentPage === totalPages && { cursor: 'not-allowed' }) }}
+        disabled={currentPage === totalPages}
+        onClick={() => handlePageChange(currentPage + 1)}
+        // 마우스를 올렸을 때 hoverStyle 적용
+        onMouseEnter={(e) => e.target.style.filter = hoverStyle.filter}
+        // 마우스가 버튼을 벗어났을 때 원래 스타일로 복원
+        onMouseLeave={(e) => e.target.style.filter = ''}
+      >
+        다시 추천받기
+      </Button>
+    </div>
   );
 };
 
