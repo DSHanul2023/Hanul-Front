b69fc828b907e4da160520c63046845d1a303829
jiho-par <hkp314518@duksung.ac.kr>
2023-09-05 14:43:53 +0900

    MinichatContentList 디자인 수정 2차

--- components/custom/sections/minichatitemcomponent.js
+++ components/custom/sections/minichatitemcomponent.js
@@ -3,18 +3,13 @@ import { Col, Container } from 'reactstrap';
 import Image from 'next/image';
 import Link from 'next/link';
 
-const MinichatItemComponent = ({ movie }) => { // movie 프로퍼티 추가
+const MinichatItemComponent = ({ movie }) => {
   return (
-    <Col xs={12} md={2} className="item">
+    <Col xs={12} md={2} className="item" style={{ height: '300px', width: '200px', cursor: 'pointer' }}>
       <Link href={`/items/${movie.movieId}`} passHref>
         <div style={{ cursor: 'pointer' }}>
           <div className="itemimg" style={{ width: '100%', height: '80%' }}>
-            <Image
-              src={movie.posterUrl}
-              alt={movie.itemNm}
-              layout="fill"
-              objectFit="cover"
-            />
+            <Image src={movie.posterUrl} alt={movie.itemNm} layout="fill" objectFit="cover" />
           </div>
           <Container className="itemsummary">
             <p className="itemtitle" style={{ marginTop: '70px', fontSize: '13px' }}>
