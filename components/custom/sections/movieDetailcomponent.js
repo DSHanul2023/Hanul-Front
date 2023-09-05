092f0de3a1ce8b11775dbf37882e1a065f0928da
seoyeong <qlqltjdud@gmail.com>
2023-09-05 15:12:32 +0900

    영화 상세페이지

--- components/custom/sections/movieDetailcomponent.js
+++ components/custom/sections/movieDetailcomponent.js
@@ -2,7 +2,8 @@ import React, { useEffect, useState } from 'react';
 import { Container, Row, Col } from 'reactstrap';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart,faVideo } from "@fortawesome/free-solid-svg-icons";
 const MovieDetailComponent = ({ movieId }) => {
   const router = useRouter();
   const [movie, setMovie] = useState(null);
@@ -29,27 +30,54 @@ const MovieDetailComponent = ({ movieId }) => {
         console.error('데이터 가져오기 오류:', error);
       });
   }, [movieId]);
-  
 
   if (!movie) {
     return <p>Loading...</p>;
   }
-
+  const keyworddiv = () =>{
+    const keywordsString = movie.keyword;
+    const keywordsArray = keywordsString.split(',');
+    
+    return keywordsArray.slice(0, 5).map((keyword, index) => (
+      <div key={index} className="tag tag-blank">
+        #{keyword.replace(/\s+/g, '_').replace('_', '')}
+      </div>
+    ));
+  }
   return (
-    <Container className="moviedetail" style={{ marginTop: "20px" }}>
-      <Row className="justify-content-center ml-4 mr-4"> {/* 중앙 정렬 및 좌우 여백 */}
-        <Col xs={12} md={3} className="poster">
+    <Container className="moviedetail">
+      <Row className="justify-content-center" style={{paddingTop:'30px',paddingBottom:'50px'}}> {/* 중앙 정렬 및 좌우 여백 */}
+        <Col xs={12} md={4} className="poster mt-2">
           <Image
             src={movie.posterUrl}
             alt={movie.itemNm}
             layout="responsive"
-            width={150}   // 이미지 너비 조정
-            height={225}  // 이미지 높이 조정
+            width={280}   // 이미지 너비 조정
+            height={400}  // 이미지 높이 조정
           />
         </Col>
-        <Col xs={12} md={9} className="info">
-          <h2>{movie.itemNm}</h2>
-          <p>{movie.itemDetail}</p>
+        <Col xs={12} md={7} className="info ml-5">
+          <div className="itemNm"><h2>{movie.itemNm}</h2></div>
+          <div className="tag_wrap tag_detail">
+            {keyworddiv()}
+          </div>
+          <div className="playdiv">
+            <button type="button" href="/" className="playbtn">
+            <FontAwesomeIcon icon={faVideo} className='mr-3'/>
+            <span>보러가기</span>
+            <div></div>
+            </button>
+            <div>
+            <button className="heartbtn">
+            <FontAwesomeIcon icon={faHeart} className="hearticon"/>
+            <span>찜</span>
+            </button>
+            </div>
+            </div>
+          <div style={{marginTop:'12px'}}>감독  {movie.director}</div>
+          <div>출연  {movie.cast}</div>
+          <div style={{marginTop:'12px'}}>{movie.itemDetail}</div>
+                    
         </Col>
       </Row>
     </Container>
