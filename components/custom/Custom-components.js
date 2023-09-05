b7b229d1e7331a4158dfd52c46011688c108df64
pjho4746 <pjho4746@gmail.com>
2023-09-05 17:33:05 +0900

    test commit

--- components/custom/Custom-components.js
+++ components/custom/Custom-components.js
@@ -6,8 +6,6 @@ import Banner2 from "../banner/Banner2";
 // sections for this page
 
 import FormBannerComponent from "./sections/formbannercomponent";
-import LoginComponent from "./sections/logincomponent";
-import ChatComponent from "./sections/chatcomponent";
 
 const CustomComponents = () => {
   const [isLocalStorageAvailable, setIsLocalStorageAvailable] = useState(false);
