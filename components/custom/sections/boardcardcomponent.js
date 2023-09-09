bc24ff58939a54fcd3456a3a2ae410e7f3b83d1d
jayy118 <hyun18_@naver.com>
2023-09-09 14:59:58 +0900

    커뮤니티 3차 수정 - board card 수정

--- components/custom/sections/boardcardcomponent.js
+++ components/custom/sections/boardcardcomponent.js
@@ -15,36 +15,40 @@ const BoardCards = ({boarditem,type,key2}) => {
                     <Col md="12">
                     <a href="#" onClick={handleClick} style={{ textDecoration: 'none', color: 'inherit' }}>
                     <div className="wrap_cont">
-                        <div className="wrap_data">
-                            <dl className="list_data mb-3">
+                        <div className='wrap_head'>
                             
-                            <dd className="txt_cate txt_cate_type1">
+                            <dl className="list_data">
+                            <dd className="txt_cate txt_cate_type1 catg">
                                 {!['A1','A2','A3','A4'].includes(type) ? (<span className="inner_data">
-                                    {item.type === '1' ? '자유' :
-                                    item.type === '2' ? '취미' :
-                                    item.type === '3' ? '동네' :
-                                    item.type === '4' ? '병원' : '기타'}
+                                    {item.type === '1' ? '자유 게시판' :
+                                    item.type === '2' ? '취미 게시판' :
+                                    item.type === '3' ? '우리 동네' :
+                                    item.type === '4' ? '병원 후기' : '기타'}
                                     </span>):(<span>&nbsp;</span>)}
                             </dd>
                             </dl>
+                            <p className="desc_tit">
+                                <span className="inner_desc_tit">{item.title}</span>
+                            </p>
+                        </div>
+                        <div className="wrap_data">
+                            
                             <dl className="list_data">
                             <dd>{item.date}</dd>
                             </dl>
+                            <span className="txt_id">by {item.author}</span>
                             <dl className="list_datac">
                             <dd>댓글 8</dd>
                             </dl>
-                            
                         </div>                            
 
-                        <p className="desc_tit">
-                            <span className="inner_desc_tit">{item.title}</span>
-                        </p>
+                        
                         </div>
                         <div className="info_g">
                         <span className="wrap_thumb">
                             <img src="/" className="thumb_profile" alt="" />
                         </span>
-                        <span className="txt_id">by {item.author}</span>
+                        
                         </div>
 
                         </a>
