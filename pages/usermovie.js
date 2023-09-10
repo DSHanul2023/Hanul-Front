0fb48c7be41e327abc5b8f22c7e86d39f13b0cb0
jiho-par <hkp314518@duksung.ac.kr>
2023-09-10 13:38:28 +0900

    대화 기반 추천 시스템 1차

---
+++ pages/usermovie.js
@@ -0,0 +1,9 @@
+import UserMovieContentListComponent from "../components/custom/sections/usermoviecontentlistcomponent";
+
+export default function UserMovie() {
+    return (
+        <div>
+            <UserMovieContentListComponent />
+        </div>
+    );
+}
