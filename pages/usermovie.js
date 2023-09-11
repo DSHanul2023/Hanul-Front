368e75a66128fd761b3a557e0790d4fdf1b0c561
chaeeunnn <90565473+chaeeunnn@users.noreply.github.com>
2023-09-11 22:58:00 +0900

    추천 페이지 수정, 로딩 추가

--- pages/usermovie.js
+++ pages/usermovie.js
@@ -1,17 +1,63 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useRouter } from "next/router";
-import UserMovieContentListComponent from "../components/UserMovieContentListComponent";
+import UserMovieContentListComponent from "../components/custom/sections/usermoviecontentlistcomponent";
+import { Container, Row, Col } from "reactstrap";
+import Contents from "../components/basic/contents";
+import MinichatContentList from "../components/custom/sections/minichatcontentlistcomponent";
+import Loading from "./loading";
 
 const UserMoviePage = () => {
   const router = useRouter();
-  const { recommendedMovies } = router.query; // 추천 영화 데이터를 가져옵니다.
+  // const { recommendedMovies } = router.query; // 추천 영화 데이터를 가져옵니다.
+  const [recommendedMovies, setRecommendedMovies] = useState([]);
+  // console.log(recommendedMovies);
+  
+  useEffect(() => {
+    const accessToken = localStorage.getItem("ACCESS_TOKEN");
+    console.log(recommendedMovies);
+    if (!accessToken) {
+      router.push("/login");
+    } else {
+      fetchRecommendMovie();
+    }
+    console.log(recommendedMovies);
+  }, []);
+
+  const fetchRecommendMovie = () => {
+    const memberId = localStorage.getItem("MEMBER_ID");
+
+    if (memberId) {
+      fetch(`http://localhost:8080/items/recommend/${memberId}`)
+        .then((response) => response.json())
+        .then((data) => {
+          setRecommendedMovies(data);
+          console.log(data);
+
+          // 추천 영화 데이터와 함께 /usermovie 페이지로 이동합니다.
+          // router.push("/usermovie", { recommendedMovies: data });
+        })
+        .catch((error) => {
+          console.error("추천 영화 불러오기 오류:", error);
+        });
+    }
+  };
 
   return (
-    <div>
-      <h1>채팅 기반 추천 영화 목록</h1>
-      {recommendedMovies && (
-        <UserMovieContentListComponent recommendedMovies={recommendedMovies} />
-      )}
+    <div className="usermovie-container">
+      {recommendedMovies.length == 0 ? <Loading/> : 
+      <>
+      <Container className="title-spacer">
+        <Row className="justify-content-center">
+          <Col md="6" className="text-center">
+            <h1 className="save-title font-bold">추천 영화</h1>
+          </Col>
+        </Row>
+      </Container>
+      <Container className="save-page-container" style={{marginBottom: "60px"}}>
+        <Contents items={recommendedMovies}/> 
+      </Container>
+      </>
+      }
     </div>
   );
 };
