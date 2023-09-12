3965069f8f35a6ac18783dc81b4ace57656143d7
jiho-par <hkp314518@duksung.ac.kr>
2023-09-12 16:21:47 +0900

    logout -> memberid의 chat message 삭제

--- pages/usermovie.js
+++ pages/usermovie.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useRouter } from "next/router";
-import UserMovieContentListComponent from "../components/UserMovieContentListComponent";
+import UserMovieContentListComponent from "../components/custom/sections/usermoviecontentlistcomponent";
 
 const UserMoviePage = () => {
   const router = useRouter();
