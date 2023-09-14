e18224ad7b7f706b40cfec37739be4b4ac90057d
seoyeong <qlqltjdud@gmail.com>
2023-09-14 20:06:40 +0900

    pet 프론트 수정

--- components/custom/sections/petImages.js
+++ components/custom/sections/petImages.js
@@ -2,3 +2,9 @@
 import default_profile from "../../../public/profile/default_profile_1.png";
 import default_profile2 from "../../../public/profile/default_profile.png";
 export const petimages = [default_profile, default_profile2, default_profile, default_profile2];
+
+const name1 = 'puppy'
+const name2 = 'puppy2'
+const name3 = 'kitty1'
+const name4 = 'kitty2'
+export const petlist = [name1,name2,name3,name4];
