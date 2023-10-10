11012e670efe92667ad4069c516d0d8f50c2181e
jiho-par <hkp314518@duksung.ac.kr>
2023-10-10 15:01:34 +0900

    색상 변경

--- components/custom/Custom-components.js
+++ components/custom/Custom-components.js
@@ -35,9 +35,9 @@ const CustomComponents = () => {
   return (
     <div>
       <Row style={{ backgroundColor: divBackgroundColor, justifyContent: "center" }}>
-        <button onClick={() => toggleSection("banner2")} style={{ border: "none", marginRight: "10px", backgroundColor: divBackgroundColor }}>1</button>
-        <button onClick={() => toggleSection("form")} style={{ border: "none", marginRight: "10px", backgroundColor: divBackgroundColor }}>2</button>
-        <button onClick={() => toggleSection("animal")} style={{ border: "none", backgroundColor: divBackgroundColor}}>3</button>
+        <button onClick={() => toggleSection("banner2")} style={{ color: "#645E4E", border: "none", marginRight: "10px", backgroundColor: divBackgroundColor }}>●</button>
+        <button onClick={() => toggleSection("form")} style={{ color: "#645E4E", border: "none", marginRight: "10px", backgroundColor: divBackgroundColor }}>●</button>
+        <button onClick={() => toggleSection("animal")} style={{color: "#645E4E",  border: "none", backgroundColor: divBackgroundColor}}>●</button>
       </Row>
 
 
